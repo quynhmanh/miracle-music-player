7b01c0fbcc9e6a66a5fef74caadf3622f17df632
manhquynhpro123 <manhquynhpro123@gmail.com>
2015-10-26 19:49:42 +0700

    main script for background

--- scripts/background/background_main.js
+++ scripts/background/background_main.js
@@ -2,8 +2,21 @@ require.config({
 	paths: {
 		jquery: "../jquery-2.1.4.min",
 		underscore: "../underscore-min",
-		backbone: "../backbone-min"
+		backbone: "../backbone-min",
+		player: "model/player",
+		song: "../common/song"
 	}
 });
 
-var a = "Hello world!";
+require([
+	'player',
+	'song'
+], function(Player, Song){
+	
+	var url = 'http://mp3.zing.vn/download/song/An-Nut-Nho-Tha-Giac-Mo-Son-Tung-M-TP/ZHJHykHNQBDShSZyZbxyvmLG';
+	
+	var song = new Song({url: url});
+	var player = new Player({currentSong: song});
+	
+	console.log(player);
+});
